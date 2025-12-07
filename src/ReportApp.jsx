aaa3f7d77b2b7b60a8c644b7e0088fb7e6ce9d2c
pennebaas <145766426+pennebaas@users.noreply.github.com>
2025-12-07 16:53:09 +0400

    Add report-only mode and ReportApp

---
+++ src/ReportApp.jsx
@@ -0,0 +1,17 @@
+// src/ReportApp.jsx
+import React from "react";
+import Level1Page from "./pages/Level1Page";
+import Level2Page from "./pages/Level2Page";
+import Level3Page from "./pages/Level3Page";
+
+const ReportApp = () => {
+  return (
+    <>
+      <Level1Page />
+      <Level2Page />
+      <Level3Page />
+    </>
+  );
+};
+
+export default ReportApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReportApp from "./ReportApp.jsx";
 
-createRoot(document.getElementById('root')).render(
+const params = new URLSearchParams(window.location.search);
+const mode = params.get("mode");
+
+// Simple debug to see what mode is being read
+console.log("App mode:", mode);
+
+const isReportMode = mode === "report";
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element with id 'root' not found in index.html");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {isReportMode ? <ReportApp /> : <App />}
+  </StrictMode>
+);
