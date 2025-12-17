289d7b0b27f4b1e2e624d7596d9b58b8d3a1ebb7
pennebaas <145766426+pennebaas@users.noreply.github.com>
2025-12-18 00:23:44 +0400

    Use relative base for Netlify/local

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/learnformance-ROI-report/",
+  base: "./", // single config: local + Netlify
 });
